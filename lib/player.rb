05a37c45ce965094ff93627697c32010366dcfc3
kirabiyakasa <icantbestoppedfools@gmail.com>
2020-10-04 22:54:54 -0400

    Replace until loop with get_input in choose_color.

--- lib/player.rb
+++ lib/player.rb
@@ -15,11 +15,7 @@ class Player
     if colors.length == 2
       puts "\nSelect a Color for Player 1."
       puts "\n1) Black    2) White"
-      answer = gets.chomp
-      until answer == "1" || answer == "2"
-        puts "\nPlease Enter a valid input."
-        answer = gets.chomp
-      end
+      answer = get_input()
       answer == "1" ? color = colors.shift : color = colors.pop
     else
       color = colors.pop
